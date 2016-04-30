77bc8e3ee80685fb84daa41c139ffe9a5c1e5a6f
twer4774 <twer4774@gamil.com>
2016-04-30 11:56:05 +0900

    Add_Get

---
+++ src/main/java/UserDao.java
@@ -0,0 +1,65 @@
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.sql.*;
+
+/**
+ * Created by TempSW on 2016. 4. 29..
+ */
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/toby", "swTemp", "password123");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/toby", "swTemp", "password123");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ?");
+
+        preparedStatement.setString(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        User user = new User();
+        user.setId(resultSet.getString("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+
+    public static void main(String[] arg) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship1");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + "등록성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회성공");
+
+    }
+
+}
